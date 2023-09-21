8b7a20d81e58b74cb5433624d7703b35f4119721
Abhinas Regmi <aviregmi2018@gmail.com>
2023-09-21 12:09:45 +0545

    Control Flow Ready

---
+++ src/soundSync/start.py
@@ -0,0 +1,24 @@
+import subprocess
+
+#check if pulseaudio is installed if not prompt to install
+
+#check if there is already virtual speaker
+
+#check if there is already virtual mic
+
+#if not create virtual speakers and mic and record their index
+
+#make virtual speaker default output
+    # - Get all device count
+    # - Make Sources default incrementally
+    # - Check if default-device name is our virtual speaker then stop
+
+#make virtual mic default input
+    # - Same steps as for speaker
+
+#start the server
+#provide url to connect
+
+#during closing remove the virutal mic and speaker with their ids.
+
+
